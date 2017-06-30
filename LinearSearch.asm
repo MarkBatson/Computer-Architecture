08cf75d599be43930793ecbc8daec4bffc967032
Mark Batson <markdbatson@gmail.com>
2017-06-30 15:26:26 -0400

    Add files via upload

---
+++ LinearSearch.asm
@@ -0,0 +1,42 @@
+# CSCI 4720 Programming Assignment 2 Linear Search
+# Mark Batson
+# Write a MIPS assembly language program that searches for a number entered by user. The number is in an array with 20 integer numbers. Print the index of the number in the array if it is found.
+
+.data
+array1: .word 15, 5, 6, 11, 17, 2, 16, 10, 1, 3, 7, 18, 4, 8, 12, 9, 20, 13, 14, 19
+str0:   .asciiz "Enter a number to find: "
+str1:   .asciiz "Number found at index: "
+
+.globl main
+
+.text
+main:	
+	la $t4, array1      # load address array1 into $t0
+	addi $t0, $t0, 4    # $t0 = 0
+	addi $t7, $t7, 0    # $t7 = 0 counter
+
+	li $v0, 4           # system call code for print_str
+	la $a0, str0        # address of string to print
+	syscall             # print the string "Enter a number to find: "
+
+	addi $v0, $zero, 5  # read integer from user
+	syscall
+	add $s0, $zero, $v0 # store integer in $s0
+
+loop1:	lw $t1, 0($t4)      # load word array1
+	beq $s0, $t1, found # branch if integer is found 
+
+	add $t4, $t4, $t0   # increment array1
+	addi $t7, $t7, 1    # $t7 = $t7 + 1
+	bne $t7, 20, loop1
+
+found: 	li $v0, 4           # system call code for print_str
+	la $a0, str1        # address of string to print
+	syscall             # print the string "Number found at index: "
+
+	li $v0, 1           # system call code for print_int
+	add $a0, $zero, $t7 # value to print
+	syscall             # print index where number is found
+
+li	$v0, 10
+	syscall
---
+++ proj1.asm
@@ -0,0 +1,46 @@
+# CSCI 4720 Programming Assignment 1
+# Mark Batson
+# Write a MIPS assembly language program that adds two arrays with ten elements (element-by-element addition) and save them in an array. Print the sum.
+
+.data
+array1: .word 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+array2: .word 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+array3: .word 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+str1:   .asciiz "Array of Sums = "
+str2:   .asciiz " "
+
+.globl main
+
+.text
+main:	
+	la $t4, array1      # load address array1 into $t0
+	la $t5, array2      # load address array2 into $t1
+	la $t6, array3      # load address array3 into $t2
+	addi $t0, $t0, 4    # $t0 = 0
+	addi $t7, $t7, 0    # $t7 = 0 counter
+
+	li $v0, 4           # system call code for print_str
+	la $a0, str1        # address of string to print
+	syscall             # print the string
+
+loop1:	lw $t1, 0($t4)      # load word array1
+	lw $t2, 0($t5)      # load word array2
+	add $t3, $t1, $t2   # add array1 to array2
+	sw $t3, 0($t6)      # store word in array3
+
+	li $v0, 1           # system call code for print_int
+	lw $a0, 0($t6)      # value to print
+	syscall             # print value
+
+	li $v0, 4           # system call code for print_str
+	la $a0, str2        # address of string to print
+	syscall             # print space
+
+	add $t4, $t4, $t0   # increment array1
+	add $t5, $t5, $t0   # increment array2
+	add $t6, $t6, $t0   # increment array3
+	addi $t7, $t7, 1    # $t0 = $t0 + 1
+	bne $t7, 10, loop1
+
+li	$v0, 10
+	syscall
